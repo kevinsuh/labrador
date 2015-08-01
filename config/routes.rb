b48ffff1f59383c64c6b2f3a83e56d24ba3b9447
kevinsuh <ksuh@dinnerlab.com>
2015-07-31 17:18:22 -0700

    routes to initial pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  
+  root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  
-  root 'application#hello'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
