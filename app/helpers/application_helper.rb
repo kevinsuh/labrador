a05a23fe0138b30e7b2877b469c9464dea6b9bb8
kevinsuh <ksuh@dinnerlab.com>
2015-07-31 18:13:57 -0700

    non-essential refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	# for title on HTML pages
+	def full_title(custom_title='')
+		base_title = 'Cardagain'
+		if custom_title.empty?
+			base_title
+		else
+			"#{custom_title} | #{base_title}"
+		end
+	end
+	
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,28 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup #this is used to set variables to be used throughout test suite
+    @base_title = "Cardagain"
+  end
+  
   test "should get home" do
     get :home
     assert_response :success
     # assert_select finds the html (aka <title>) selector
-    assert_select "title", "Home | Cardagain"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Cardagain"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-  	assert_select "title", "About | Cardagain"
+  	assert_select "title", "About | #{@base_title}"
   end
 
 end
