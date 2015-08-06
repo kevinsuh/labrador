1f971084868ac5934dfb9cbcebda1087db14a8a7
kevinsuh <ksuh@dinnerlab.com>
2015-08-05 21:08:51 -0500

    prepare for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,15 +64,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://dev-cardagain.herokuapp.com/'
+  host = "https://dev-cardagain.herokuapp.com/"
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    authentication: plain,
+    address: "smtp.sendgrid.net",
+    port: "587",
+    authentication: :plain,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
+    domain: "heroku.com",
     enable_starttls_auto: true
   }
 
