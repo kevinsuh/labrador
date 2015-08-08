db38c8b56fadccfc32a85b4f6cee46d40639fa90
kevinsuh <ksuh@dinnerlab.com>
2015-08-08 12:38:10 -0500

    jQuery bug fix with waitlist email input

--- app/assets/javascripts/splash_page.js
+++ app/assets/javascripts/splash_page.js
@@ -1,11 +1,16 @@
 $(document).on('ready page:load', function() {
 
+	// waitlist input functions
 	$('#waitlist_email').focus(function() {
+		$(this).addClass('active')
 		$('#submit_waitlist').addClass('active')
 	});
 
 	$('#waitlist_email').blur(function() {
-		$('#submit_waitlist').removeClass('active')
+		if (!$(this).val()) {
+			$(this).removeClass('active')
+			$('#submit_waitlist').removeClass('active')
+		}
 	});
 
 });
