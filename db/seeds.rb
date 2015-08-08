df339c9cc7e13a0350603eb5ceb18b601707f5d2
kevinsuh <ksuh@dinnerlab.com>
2015-08-08 00:12:43 -0500

    get rid of db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,22 @@
 # real user/login that we want
-User.create!(name: 'Kevin Suh',
-						 email: 'kevinsuh34@gmail.com',
-						 password: 'kevinsuh',
-						 password_confirmation: 'kevinsuh',
-						 is_admin: true,
-						 is_activated: true,
- 						 activated_at: Time.zone.now )
+# User.create!(name: 'Kevin Suh',
+# 						 email: 'kevinsuh34@gmail.com',
+# 						 password: 'kevinsuh',
+# 						 password_confirmation: 'kevinsuh',
+# 						 is_admin: true,
+# 						 is_activated: true,
+#  						 activated_at: Time.zone.now )
 
-99.times do |n|
-	name = Faker::Name.name
-	email = "testemail-#{n+1}@mail.com"
-	password = "password"
+# 99.times do |n|
+# 	name = Faker::Name.name
+# 	email = "testemail-#{n+1}@mail.com"
+# 	password = "password"
 
-	User.create!( name: name,
-								email: email,
-								password: password,
-								password_confirmation: password,
-								is_activated: true,
- 								activated_at: Time.zone.now )
+# 	User.create!( name: name,
+# 								email: email,
+# 								password: password,
+# 								password_confirmation: password,
+# 								is_activated: true,
+#  								activated_at: Time.zone.now )
 
-end
+# end
