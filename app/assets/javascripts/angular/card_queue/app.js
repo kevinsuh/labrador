85317d34e12c9535586b484b53c0fa52afa579ce
kevinsuh <ksuh@dinnerlab.com>
2015-08-21 23:32:47 -0500

    separate out queue steps w/ custom directive

--- app/assets/javascripts/angular/card_queue/app.js
+++ app/assets/javascripts/angular/card_queue/app.js
@@ -30,23 +30,38 @@
 
       .state('home.app.step1', {
         url: '',
+        replace: true,
         templateUrl: "angular/card_queue/form_step_one.html"
       })
 
       .state('home.app.step2', {
+        replace: true,
         templateUrl: "angular/card_queue/form_step_two.html"
       })
 
       .state('home.app.step3', {
+        replace: true,
         templateUrl: "angular/card_queue/form_step_three.html"
       })
 
       .state('home.app.step4', {
+        replace: true,
         templateUrl: "angular/card_queue/form_step_four.html"
       });
 
     $urlRouterProvider.otherwise('/');
 
+  });
+
+  // temp spot for card queue directives
+  // this directive holds the links for iterating through the form
+  app.directive("nextStep", function() {
+    return {
+      scope: {
+        currentState: "=currentState"
+      },
+      templateUrl: "angular/card_queue/_form_step_links.html"
+    };
   });
 
 })();
