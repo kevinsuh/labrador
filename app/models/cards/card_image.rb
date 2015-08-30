48bdabaf423722ada23a1a061128b51a4950d74a
kevinsuh <ksuh@dinnerlab.com>
2015-08-29 20:52:52 -0500

    upload card image from database

--- app/models/cards/card_image.rb
+++ app/models/cards/card_image.rb
@@ -1,4 +1,11 @@
 class CardImage < ActiveRecord::Base
   belongs_to :card
   mount_uploader :picture, PictureUploader
+
+
+  private 
+
+  def card_image_params
+  	params.require(:card_image).permit(:picture)
+  end
 end
