2924a8f668e208c7121d31e782696d929eb38f3e
kevinsuh <ksuh@dinnerlab.com>
2015-09-04 14:32:47 -0500

    stripe checkout widget

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,10 +3,13 @@ class ChargesController < ApplicationController
 
 	# credit card form
 	def new
+		@amount = 5
 	end
 
 	# charge credit card
 	def create
+		flash[:success] = "You're cards have been purchased. Thank you!"
+		redirect_to root_url
 	end
 
 end
