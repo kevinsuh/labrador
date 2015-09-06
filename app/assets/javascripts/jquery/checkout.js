bee864b1a4b527fdfef021c130c503af48efbac5
kevinsuh <ksuh@dinnerlab.com>
2015-09-05 22:16:04 -0500

    show and select saved addresses

--- app/assets/javascripts/jquery/checkout.js
+++ app/assets/javascripts/jquery/checkout.js
@@ -3,7 +3,7 @@ $(document).on('ready page:load', function() {
 	// waitlist input functions
 	$('#new_address').click(function(e) {
 		e.preventDefault();
-		$('div.address').fadeToggle();
+		$('.address_form_div').fadeToggle();
 	});
 
 });
