35d76e6399fa58b58f9d3a833ad97c766e1c8338
kevinsuh <ksuh@dinnerlab.com>
2015-09-20 18:42:46 -0500

    side links show active

---
+++ app/assets/javascripts/helpers/logged_in_home.js
@@ -0,0 +1,31 @@
+/* generic method to post request via javascript */
+function post(path, params, method) {
+	method = method || "post"; // Set method to post by default if not specified.
+
+	// The rest of this code assumes you are not using a library.
+	// It can be made less wordy if you use one.
+	var form = document.createElement("form");
+	form.setAttribute("method", method);
+	form.setAttribute("action", path);
+
+	for(var key in params) {
+		if(params.hasOwnProperty(key)) {
+			var hiddenField = document.createElement("input");
+			hiddenField.setAttribute("type", "hidden");
+			hiddenField.setAttribute("name", key);
+			hiddenField.setAttribute("value", params[key]);
+
+			form.appendChild(hiddenField);
+		 }
+	}
+	document.body.appendChild(form);
+	form.submit();
+}
+
+// current workaround to have highlighted side links
+function makeStateActive(stateName) {
+	$('.state_link').removeClass('active');
+	if (stateName == 'my_people.app.card_view') {
+		$("#my_people").addClass('active');
+	}
+}
