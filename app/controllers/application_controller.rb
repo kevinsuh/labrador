21b4dbb2ac5503ceedbc71ecf2c85ef2fe89a97d
kevinsuh <ksuh@dinnerlab.com>
2015-11-09 13:18:33 -0600

    footer update for mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   include StaticPagesHelper
+  include ApplicationHelper
 
   protected
   	def require_login
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,9 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+Rails.application.assets.context_class.class_eval do
+  include ApplicationHelper
+  include ActionView::Helpers
+  include Rails.application.routes.url_helpers
+end
