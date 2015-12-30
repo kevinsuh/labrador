35677e1a2eba51122a74483f2a993f11bede2c96
kevinsuh <ksuh@dinnerlab.com>
2015-12-30 11:05:10 -0800

    add username to users table in DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123010757) do
+ActiveRecord::Schema.define(version: 20151230190405) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "first_name"
@@ -278,6 +278,7 @@ ActiveRecord::Schema.define(version: 20151123010757) do
     t.integer  "birth_day"
     t.integer  "birth_year"
     t.string   "phone_number"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
